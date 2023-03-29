db3a30342759bf8e26adf16c2ded98eb9fdd04c3
zuzeyka <stepan.tarasenko.26@gmail.com>
2023-03-29 19:09:18 +0300

    Init repo

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Intro()
+        {
+            ViewBag.data = "Data in ViewBag"; // способы передачи данных
+            ViewData["data"] = "Data in ViewData"; // к представлению
+            return View();
+        }
+
+        public IActionResult URL()
+        {
+            return View();
+        }
+
+        public IActionResult Scheme()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
