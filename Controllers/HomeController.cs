ee42c7f427686f604986c4ec7f3c6b8d6e181516
zuzeyka <stepan.tarasenko.26@gmail.com>
2023-04-04 17:42:18 +0300

    Added display templates and tag-helpers pages and user registration field

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -28,6 +28,31 @@ namespace WebApplication1.Controllers
             return View();
         }
 
+        public IActionResult TagHelpers()
+        {
+            return View();
+        }
+
+        public IActionResult DisplayTemplates()
+        {
+            Models.Home.PassDataModel model = new()
+            {
+                Header = "DisplayTemplates",
+                Title = "Data Display Templates",
+                Products = new()
+                {
+                    new() { Name = "Зарядний кабель",       Image = "1.png", Price = 210 },
+                    new() { Name = "Маніпулятор 'миша'",    Image = "2.png", Price = 399.50 },
+                    new() { Name = "Наліпка 'Smiley'",      Image = "3.png", Price = 2.95 },
+                    new() { Name = "Серветки для монітору", Image = "4.png", Price = 100 },
+                    new() { Name = "USB ліхтарик",          Image = "5.png", Price = 49.50 },
+                    new() { Name = "Аккумулятор ААА",       Image = "6.png", Price = 280 },
+                    new() { Name = "ОС Windows Home",       Image = "7.png", Price = 1250 },
+                }
+            };
+            return View(model);
+        }
+
         public IActionResult PassData()
         {
             Models.Home.PassDataModel model = new()
---
+++ Controllers/UserController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Registration()
+        {
+            return View();
+        }
+    }
+}
